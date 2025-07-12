513d0d5bd8f4d8bf0f429a6c78399d3e2e777826
Jean Roussie <jean.roussie@free.fr>
2025-07-12 07:32:23 +0200

    Update fiche_effacer.php
    Nettoyage et sécurisation du code

--- fiche_effacer.php
+++ fiche_effacer.php
@@ -1,118 +1,114 @@
 <?php
-  session_start();
-  # #############################
-  # Récupère les données communes
-  # #############################
-  
-  require "config.php";
-  include $root."includes/header.php";  include $root."includes/common.php";;
-  include('includes/init_donnees.php');
-  
-  # #############################
-  # verification connexion
-  # #############################
-  
-  #
-#var_dump($_POST); # <------------------------- VERIF
-  #  echo "<br>\$_SESSION :"; var_dump($_SESSION)."<br>"; # <------------------------- VERIF
-  #
-  
-  if (count($_SESSION) > 0)	
-    {
-      $connect = "Connecté comme ".$_SESSION['pseudo']." : ";
-    }
-  else
-    {
-      $connect = "Déconnecté";
-    }
-  
-  ##############################
-  # INITIALISATION VARIABLES
-  #############################
-  
-  # #############################
-  # Création de la connection à la base
-  # #############################
-  $id = 0;
-  if (isset($_POST['id'])) {$id = $_POST['id'];};
-  
-  if ($id == 0) {
+session_start();
+// #############################
+// Load common data and configuration
+// #############################
+require "config.php";
+require $root."includes/common.php";
+require $root."includes/init_donnees.php";
+
+// #############################
+// Verify user session
+// #############################
+$isLoggedIn = !empty($_SESSION['pseudo']);
+$connect = $isLoggedIn ? "Connecté comme ".htmlspecialchars($_SESSION['pseudo']) : "Déconnecté";
+
+// #############################
+// Initialize variables
+// #############################
+$id = isset($_POST['id']) ? (int)$_POST['id'] : 0;
+$bouton = '';
+$avis = '';
+
+// #############################
+// Database operations
+// #############################
+if ($id == 0) {
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    
     $connection = new mysqli($host, $username, $password, $dbname);
-    
-    # #############################
-    # initialisation des listes 
-    # #############################
-    
-    # Récupération des lieux
-    
-    $statement = $connection->query("SELECT MAX(id) FROM matos");
-    $idmax = $statement->fetch_all(MYSQLI_ASSOC);
-  #var_dump($idmax); # <------------------------- VERIF
-  #print_r($idmax[0]['MAX(id)']);
-  $id = $idmax[0]['MAX(id)'];
-    if (($id == 0) or (isset($_POST['supprimer']))) {
-      $statement = $connection->query("DELETE FROM matos WHERE id = ".$id);
-      if ($id == 0) {
-        $statement = $connection->query("ALTER TABLE matos AUTO_INCREMENT = ".$id);};
-      #
-    };
-  };
-  #
-  $bouton='';
-  if ($id == 0) {$bouton = '';}; $avis='';
-  if (($id > 0) and (isset($_POST['supprimer'])))
-    { $bouton = "  <a href='liste_selection.php'><input type='button' value='Abandonner' ></a>";};
-    {$avis = "Attention ! Voulez-vous vraiment effacer le fiche ?";}
-  if (isset($_POST['confirmer']))
-    {"La fiche a été supprimée, cliquer sur le bouton 'Abandonner' pour sortir." ;};  
+
+    // Get the maximum ID from matos table
+    $statement = $connection->query("SELECT MAX(id) AS max_id FROM matos");
+    $idmax = $statement->fetch_assoc();
+    $id = (int)$idmax['max_id'];
+
+    // Delete record if ID is 0 or delete button was pressed
+    if ($id == 0 || isset($_POST['supprimer'])) {
+        $statement = $connection->query("DELETE FROM matos WHERE id = ".$id);
+        if ($id == 0) {
+            $statement = $connection->query("ALTER TABLE matos AUTO_INCREMENT = ".$id);
+        }
+    }
+    $connection->close();
+}
+
+// Set button and message based on conditions
+if ($id > 0 && isset($_POST['supprimer'])) {
+    $bouton = "<a href='liste_selection.php'><input type='button' value='Abandonner'></a>";
+    $avis = "Attention ! Voulez-vous vraiment effacer la fiche ?";
+}
+
+if (isset($_POST['confirmer'])) {
+    $avis = "La fiche a été supprimée, cliquer sur le bouton 'Abandonner' pour sortir.";
+}
+
+// #############################
+// HTML Output
+// #############################
+require $root."includes/header.php";
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Suppression de fiche</title>
+</head>
 <body>
-  <p>    
-    <?php 
-      if (count($_SESSION) > 0)
-    { ?>
-    <form action="index.php" method="post"><?php echo $connect." "; ?><input type='submit' name="deconnexion" value="Déconnexion"></form><?php  
-      }
-      else 
-    { ?>
-    <a href=login.php>Connection</a><?php
-      }
-    ?>
-  </p>
-  <hr>
-  <table>
-    <tr>
-      <td> <h1>Gestionnaire EPI</h1></td><td rowspan=2><img src="images/logo.png" width="200"></td>
-    </tr>
-    <tr><td><h2>Périgord Escalade</h2></td></tr>
-  </table>
-  <hr>
-  <!-- Test si connecté debut -->
-  <?php if (count($_SESSION) > 0)
-    { 
-  ?>
-  <!---->
-  <p><?php echo $avis ?></p>
-  
-  <form  enctype="multipart/form-data" method='post' action="fiche_effacer.php">
+    <p>
+        <?php if ($isLoggedIn): ?>
+            <form action="index.php" method="post">
+                <?= $connect ?>
+                <input type="submit" name="deconnexion" value="Déconnexion">
+            </form>
+        <?php else: ?>
+            <a href="login.php">Connexion</a>
+        <?php endif; ?>
+    </p>
+    <hr>
     <table>
-      <tbody>
-      </tbody>
-      <tfoot>
-        <tr><td><?php echo $bouton?>
-            <input type="submit" name="supprimer" value="Confirmer">
-          </td>
+        <tr>
+            <td><h1>Gestionnaire EPI</h1></td>
+            <td rowspan="2"><img src="images/logo.png" width="200" alt="Logo"></td>
         </tr>
-      </tfoot>
+        <tr>
+            <td><h2>Périgord Escalade</h2></td>
+        </tr>
     </table>
-  </form>
-  <!-- Test si connecté fin et pied de page -->
-  <?php }
-    else { ?>
-  <p>Tu n'es pas connecté</p>
-  <?php 
-    };
-  ?>
-  <?php require $root."includes/footer.php"; ?>
+    <hr>
+
+    <?php if ($isLoggedIn): ?>
+        <p><?= htmlspecialchars($avis) ?></p>
+        
+        <form method="post" action="fiche_effacer.php">
+            <table>
+                <tbody>
+                    <!-- Additional form fields can be added here if needed -->
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td>
+                            <?= $bouton ?>
+                            <input type="hidden" name="id" value="<?= $id ?>">
+                            <input type="submit" name="confirmer" value="Confirmer">
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </form>
+    <?php else: ?>
+        <p>Vous n'êtes pas connecté</p>
+    <?php endif; ?>
+
+    <?php require $root."includes/footer.php"; ?>
+</body>
+</html>
