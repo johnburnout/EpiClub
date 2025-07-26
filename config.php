4db3a1f902683db0885f34cdf126a2d1c6ec2fa7
Jean Roussie <jean.roussie@free.fr>
2025-07-26 18:14:01 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+  
+  /**
+  * Configuration pour database connection
+  *
+  */
+  
+  $host       = "localhost";
+  $username = "votre login mySQL";
+  $password = "votre mdp mySQL";
+  $dbname     = "epi";
+  
+  $images     = "/images/";
+  $enregistrements = "../enregistrements";
+  $root       = $_SERVER['DOCUMENT_ROOT'].'/epi/';
+?>
