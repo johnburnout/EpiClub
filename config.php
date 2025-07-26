f5b803439f99df742ac692550634db44ee303839
Jean Roussie <jean.roussie@free.fr>
2025-07-26 18:06:08 +0200

    Update config.php

--- config.php
+++ config.php
@@ -6,11 +6,8 @@
   */
   
   $host       = "localhost";
-  //$host= "ns3126637.ip-51-68-33.eu";
-  //$username   = "jean";
-  //$password   = "xF]*/Jeul2QDGUr/";
-  $username = "perigord-escalade_club";
-  $password = "uQIGkqR7PriOskl";
+  $username = "login mySQL";
+  $password = "mdp mySQL";
   $dbname     = "epi";
   
   $images     = "/images/";
