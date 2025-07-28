854a98e0356fc258be8a43692995e03d9b2f741f
Jean Roussie <jean.roussie@free.fr>
2025-07-28 10:00:56 +0200

    Add files via upload

---
+++ config.modele.php
@@ -0,0 +1,18 @@
+<?php
+  
+  /**
+  * Configuration pour database connection
+  *
+  */
+  
+  $host       = "localhost";
+  $username = "votre login mysql";
+  $password = "votre mdp mysql";
+  $dbname     = "epi";
+  
+  $root       = __DIR__.'/';
+  
+  $dossier_images     = $root."images/";
+  $dossier_enregistrements = $root."enregistrements/";
+  $dossier_factures = $root."factures/";
+?>
